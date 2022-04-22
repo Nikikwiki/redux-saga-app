f84cd715d8be0ce09e3bb5fb1aee1baa05e8b10b
chn <nikikwiki@mail.ru>
2022-04-22 13:57:13 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import { useSelector, useDispatch } from "react-redux"
-import { getNews } from "./redux/actions/actionCreator"
-import News from "./components/news/news";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 
-const App = () => {
-    const latestNews = useSelector(store => store.news.latestNews || []);
-    const popularNews = useSelector(store => store.news.popularNews || [])
-    const { latestNewsError, popularNewsError } = useSelector(store => store.errors || {})
-    const dispatch = useDispatch();
-
-    const handleNews = () => {
-        dispatch(getNews());
-    }
-
-    return (
-        <div>
-            <button onClick={() => handleNews()}>GetNews</button>
-            <News news={latestNews} title="Latest news"  error={latestNewsError}/>
-            <News news={popularNews} title="Popular news" error={popularNewsError} />
-        </div>
-    )
-}
+const App = ({ children }) => (
+  <>
+    <Header />
+    <main>
+      {children}
+    </main>
+    <Footer />
+  </>
+);
 
 export default App;
