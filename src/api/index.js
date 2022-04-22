5becd9d0f122d5cf80d31205508ef464a20f6d7c
chn <nikikwiki@mail.ru>
2022-04-22 15:33:30 +0300

    iteraction

--- src/api/index.js
+++
@@ -1,9 +0,0 @@
-export const getLatestNews = async(searchQery) => {
-    const request = await fetch(`https://hn.algolia.com/api/v1/search?query=${searchQery}&hitsPerPage=10&page=0`);
-    return await request.json();
-}
-
-export const getPopularNews = async() => {
-    const request = await fetch(`https://hn.algolia.com/api/v1/search?query=&hitsPerPage=10&page=0`);
-    return await request.json();
-}
--- src/components/footer/footer.js
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-  <footer>
-    <h3>Created by Nik</h3>
-  </footer>
-);
-
-export default Footer;
